9dc7de29e9847659cfc876493f949a070014127f
shakib-ir <rashidshakib56@gmail.com>
2025-03-05 17:52:50 +0600

    My first java code

---
+++ firstpush.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class firstpush {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        for (int i = 0;i<10;i++) {
+            System.out.println("Hello World");
+        }
+
+        List<Integer> list = new ArrayList<Integer>();
+    }
+}
